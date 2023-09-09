37110cf00ed3505f10aaff7a602855dc1326bec0
Fred Robins <fredrobins619@outlook.com>
2023-09-09 15:29:55 +0000

    Result components' design updated

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,16 +1,16 @@
 export default function Result() {
     return (
-        <div className="flex flex-col bg-cobalt-blue text-light-lavender font-hanken p-9 items-center text-center rounded-b-3xl">
-            <p className="mb-4 ">Your Result</p>
+        <div className="flex flex-col items-center justify-between text-center bg-gradient-to-br from-special-1 to-special-2 text-light-lavender font-hanken p-9 rounded-b-3xl md:w-full md:rounded-3xl md:p-8">
+            <p className="mb-8 text-2xl">Your Result</p>
 
-            <div className="font-bold rounded-full w-32 h-32 bg-gradient-to-b from-circle-violet-blue from-30% to-circle-persian-blue flex flex-col justify-center items-center">
-                <h1 className="text-5xl text-white">76</h1>
-                <span>of 100</span>
+            <div className="flex flex-col items-center justify-center w-48 h-48 font-extrabold rounded-full bg-gradient-to-b from-circle-persian-blue to-circle-violet-blue">
+                <h1 className="text-white text-7xl">76</h1>
+                <span className='text-light-lavender'>of 100</span>
             </div>
 
-            <div className="p-4">
-                <h2 className="text-2xl text-white font-bold">Great</h2>
-                <p>You scored higher than 65% of the people who have taken these tests.</p>
+            <div className="mt-8">
+                <h2 className="text-3xl font-medium text-white">Great</h2>
+                <p className="p-2 text-lg">You scored higher than 65% of the people who have taken these tests.</p>
             </div>
         </div>
     )
