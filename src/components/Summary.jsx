c40a7a01ded152da327dd8d06ea3d59061f9f13d
Fred Robins <fredrobins619@outlook.com>
2023-09-21 08:05:24 +0000

    Update image path (src) and alt value

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,5 +1,10 @@
 import Tests from "./Tests";
 import hola from '../.././data.json'
+import reactionImg from './../../assets/images/icon-reaction.svg'
+import memoryImg from './../../assets/images/icon-memory.svg'
+import verbalImg from './../../assets/images/icon-verbal.svg'
+import visualImg from './../../assets/images/icon-visual.svg'
+
 
 export default function Summary() {
 
@@ -9,32 +14,32 @@ export default function Summary() {
             <div>
                 <Tests 
                 className='bg-alpha-light-red text-light-red' 
-                src={hola[0].icon} 
-                alt={`${hola[0].icon}`} 
+                src={reactionImg} 
+                alt={`${hola[0].category} icon`} 
                 category={hola[0].category} 
                 score={hola[0].score}
                 />
 
                 <Tests 
                 className='bg-alpha-orangey-yellow text-orangey-yellow' 
-                src={hola[1].icon} 
-                alt={`${hola[1].icon}`} 
+                src={memoryImg} 
+                alt={`${hola[1].category} icon`} 
                 category={hola[1].category} 
                 score={hola[1].score}
                 />
                 
                 <Tests 
                 className='bg-alpha-green-teal text-green-teal' 
-                src={hola[2].icon} 
-                alt={`${hola[2].icon}`} 
+                src={verbalImg} 
+                alt={`${hola[2].category} icon`} 
                 category={hola[2].category} 
                 score={hola[2].score}
                 />
 
                 <Tests 
                 className='bg-alpha-cobalt-blue text-cobalt-blue' 
-                src={hola[3].icon} 
-                alt={`${hola[3].icon}`} 
+                src={visualImg} 
+                alt={`${hola[3].category} icon`} 
                 category={hola[3].category} 
                 score={hola[3].score}
                 />
